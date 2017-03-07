fc753269ced89fd5cdfea43653da1037c2e42ec7
krishnapriya <krishnapriyakamesh@gmail.com>
2017-03-07 10:39:25 -0600

    working on  dropping populated to table

--- main.js
+++ main.js
@@ -5,9 +5,17 @@ const db = new sqlite3.Database('example.sqlite');//db is the database object
 const dropEmployees =()=>{
   db.run('DROP TABLE employees');
 };
-//dropEmployees();
+// dropEmployees();
 
 //CREATE A TABLE USING JS
-db.run("CREATE TABLE IF NOT EXISTS employees (id INT,first TEXT,last TEXT,salary NUMBER(6,2))");
+db.run("CREATE TABLE IF NOT EXISTS employees (id INT,first TEXT,last TEXT,salary INT,Dept TEXT)");
 
-//db.run('INSERT INTO employees VALUES (1,"Priya","Kamesh",60000.00)') //NOT SO EFFICIENT
+// db.run('INSERT INTO employees VALUES (1,"Priya","Kamesh",600000)') //NOT SO EFFICIENT
+
+const populateEmployees =()=>{
+ const {list} = require('./employees.json');
+ list.forEach(each => {
+  db.run(`INSERT INTO employees VALUES (${each.id}, "${each.firstName}", "${each.lastName}",${each.salary},"${each.Dept}")`)
+ });
+};
+populateEmployees();
